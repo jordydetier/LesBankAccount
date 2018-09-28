5c816d3d07e70e9d73e26d73a46f39e5855208d9
Jordy De Tier <jordy.detier@student.hogent.be>
2018-09-28 09:46:35 +0200

    creëer klasse BankAccount

---
+++ Banking/Models/Domain/BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Banking.Models.Domain
+{
+    class BankAccount
+    {
+        // private string _accountNumber;
+        #region Fields
+   
+        private decimal _balance;
+
+        #endregion
+
+
+        #region Properties
+        public string AccountNumber { get;  set; }
+
+
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+             set
+            {
+                if (value < 0)
+                    throw new ArgumentException("No negative Balance!");
+                _balance = value;
+            }
+
+        }
+        #endregion
+        #region Constructors
+        public BankAccount()
+        {
+
+        }
+        
+        public BankAccount(String accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+        public BankAccount(String accountNumber, decimal balance) : this(accountNumber)
+        {
+            Balance = balance;
+        } 
+        #endregion
+
+        #region Methods
+        public void Deposit(decimal amount, int nrOfTimes=1)
+        {
+            Balance += amount * nrOfTimes;
+        }
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        } 
+        #endregion
+    }
+}
--- Banking/Program.cs
+++ Banking/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking.Models.Domain;
+using System;
 
 namespace Banking
 {
@@ -6,7 +7,24 @@ namespace Banking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Object Initializer
+            //BankAccount anotherBA = new BankAccount()
+            //{
+            //    AccountNumber = "123-12312334-990",
+            //    Balance = 200
+            //};
+
+            BankAccount myBa = new BankAccount("123-12312312-99", 50);
+
+            Console.WriteLine($"Account number is {myBa.AccountNumber}.");
+            
+            Console.WriteLine($"Balance is {myBa.Balance} Euro");
+            myBa.Deposit(200);
+            Console.WriteLine($"Balance is {myBa.Balance} Euro");
+            myBa.Withdraw(50);
+            Console.WriteLine($"Balance is {myBa.Balance} Euro");
+            Console.ReadKey();
+
         }
     }
 }
